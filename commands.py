d0055a496450158240b58378c2d467e7dcad5be0
monstermind1 <dhanajyagupta@gmail.com>
2026-01-13 10:40:10 +0530

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,15 @@
+COMMANDS = {
+    "/help": "🔥 /ping /stats /music /funny /masti /welcome",
+    "/ping": "✅ Bot 100% LIVE! 🔥",
+    "/stats": "Stats...", 
+    "/music": "🎵🎶 Music ON! 🎧",
+    "/funny": "😂😂 Hahaha mast!",
+    "/masti": "🎉🥳 Party time!",
+    "/welcome": "Test welcome 👋"
+}
+
+AUTO_REPLIES = {
+    "hi": "Hey bro! 😎",
+    "hello": "Namaste! 🔥",
+    "kya": "Sab theek! 😄"
+}

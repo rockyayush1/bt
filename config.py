75d937a328d23ceb45b6b884965f4036333bb1f6
monstermind1 <dhanajyagupta@gmail.com>
2026-01-13 10:39:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# 🔥 EDIT THESE 3 LINES ONLY
+USERNAME = "YOUR_USERNAME_HERE"
+PASSWORD = "YOUR_PASSWORD_HERE"
+GROUP_IDS = ["1234567890"]
+
+WELCOME_MSGS = [
+    "Hey @user welcome bro! 🔥",
+    "New member! Masti shuru! 🎉",
+    "Hello bhai! Enjoy! 😎"
+]
+
+DELAY = 2
+POLL = 5

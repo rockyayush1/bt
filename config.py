83681862316bea20a6c4e871f8ff6537d14b4989
rockyayush1 <dhanajyagupta@gmail.com>
2026-01-13 18:34:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 SESSION_TOKEN = "56748960230%3A3cDQEAxNpjK1Ak%3A19%3AAYhfCeVeCOMFqt90z10vgTBJQV4CW1sso1ZZdXgxag"
-GROUP_IDS = ["29871068355871187"]  # 👈 YE BADLO
-ADMIN_USERS = ["vimalkumaw4"]    # 👈 YE BADLO
+GROUP_IDS = ["29871068355871187"]  # 👈 YE BADALO
+ADMIN_USERS = ["vimalkumaw4"]    # 👈 YE BADALO
 
 WELCOME_MSGS = [
     "Hey @user welcome bro! 🔥",
-    "New member! Masti ON! 🎉",
-    "Hello bhai! Enjoy! 😎"
+    "New member! Masti ON! 🎉", 
+    "Hello bhai! Group enjoy! 😎"
 ]
 
-DELAY = 2
+DELAY = 3
 POLL = 5
